5540faf70a498d1415501c1b0e2573c846e93545
ishelly1997 <ishelly1997@gmail.com>
2022-05-26 23:16:16 -0400

    pull

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo1 from '../../assets/passport-plane.png'
 import logo2 from '../../assets/dinner-plate.png'
+import logo3 from '../../assets/Bootcamp.gif'
 import '../../assets/style.css'
 
 function Projects() {
@@ -16,6 +17,15 @@ function Projects() {
     </div>
     <div id='project-card'>
 
+    <h1>Bootcamp Social</h1>
+    <img id="logo3" src={logo3} alt="logo"/>
+    <div/>
+    <a href='https://bootcamp-social.herokuapp.com/'>Deployed App</a>
+    <div/>
+    <a href='https://github.com/ishelly1997/bootcamp-social'>Github Repository</a>
+    </div>    
+    <div id='project-card'>
+
         <h1>Restaurant Rater</h1>
         <img id="logo2" src={logo2} alt="logo"/>
         <div/>
@@ -23,6 +33,7 @@ function Projects() {
         <div/>
         <a href='https://github.com/cathmcneel/restaurant-rater'>Github Repository</a>
       </div></div>
+      
     );
 }
 
